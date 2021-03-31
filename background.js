61eafdddf380795e51e1a83ca739433de6f1e30e
Phoenix <v4gr33n@hotmail.com>
2021-03-30 22:48:38 -0600

    added test

---
+++ background.js
@@ -0,0 +1,10 @@
+function greennenPage() {
+    document.body.style.backgroundColor = "green";
+}
+
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: greennenPage
+    })
+})
